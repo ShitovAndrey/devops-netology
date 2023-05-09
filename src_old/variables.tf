cfd28fdb1e15ae6d1312d16a0af6e5610e9865db
ShitovAndrey <ShitovAndrey@example.ru>
2023-05-09 09:37:57 +0300

    WIP

--- src_old/variables.tf
+++ src_old/variables.tf
@@ -28,5 +28,32 @@ variable "default_cidr" {
 variable "vpc_name" {
   type        = string
   default     = "develop"
-  description = "VPC network&subnet name"
+  description = "VPC network & subnet name"
+}
+
+###ssh vars
+variable "vm_metadata" {
+  type = map
+  default = {
+    ssh-keys           = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMnj+Iurh5zYW5983pk5CGye+SLfKpRPGzYUJrf2ikl8 andy@ubu18"
+    ssh-user           = "ubuntu"
+    serial-port-enable = 1
+  }
+}
+
+###yandex_compute_image
+variable "compute_image_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+###for locals
+variable "env" {
+  type        = string
+  default     = "develop"
+}
+
+variable "project" {
+  type    = string
+  default = "platform"
 }
