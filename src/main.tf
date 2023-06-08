f9de3392ef0bf2cb23d3a315e7429f7fd75ca8cf
ShitovAndrey <ShitovAndrey@example.ru>
2023-06-08 08:49:48 +0300

    fix: checkov and tflint

--- src/main.tf
+++ src/main.tf
@@ -15,7 +15,7 @@ module "vpc_dev" {
 #                     VM
 ########################################################
 module "test-vm" {
-  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=1.0.0"
   env_name        = "develop"
   network_id      = module.vpc_dev.vpc_id
   subnet_zones    = ["ru-central1-a"]
@@ -23,8 +23,10 @@ module "test-vm" {
   instance_name   = "web"
   instance_count  = 1
   image_family    = "ubuntu-2004-lts"
-  public_ip       = true
-  
+  public_ip       = false
+  security_group_ids = [yandex_vpc_security_group.example.id]
+
+
   metadata = {
       user-data          = data.template_file.cloudinit.rendered
       serial-port-enable = 1
